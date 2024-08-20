e9fbed1e20c22c003fc5e1baf68e3df8392d9b6e
Chronicle20 <a.chronicle.20@gmail.com>
2024-08-20 12:32:15 -0400

    Add handling for adding/updating inventory items. Item movement is stubbed.

--- atlas.com/channel/character/processor.go
+++ atlas.com/channel/character/processor.go
@@ -1,9 +1,14 @@
 package character
 
 import (
+	"atlas-channel/character/inventory"
+	"atlas-channel/character/inventory/equipable"
+	"atlas-channel/character/inventory/item"
 	"atlas-channel/kafka/producer"
 	"atlas-channel/socket/model"
 	"atlas-channel/tenant"
+	"errors"
+	model2 "github.com/Chronicle20/atlas-model/model"
 	"github.com/Chronicle20/atlas-rest/requests"
 	"github.com/opentracing/opentracing-go"
 	"github.com/sirupsen/logrus"
@@ -28,5 +33,50 @@ func Move(l logrus.FieldLogger, span opentracing.Span, tenant tenant.Model) func
 		if err != nil {
 			l.WithError(err).Errorf("Unable to distribute character movement to other services.")
 		}
+	}
+}
+
+func GetEquipableInSlot(l logrus.FieldLogger, span opentracing.Span, tenant tenant.Model) func(characterId uint32, slot int16) model2.Provider[equipable.Model] {
+	return func(characterId uint32, slot int16) model2.Provider[equipable.Model] {
+		// TODO this needs to be more performant
+		c, err := GetByIdWithInventory(l, span, tenant)(characterId)
+		if err != nil {
+			return model2.ErrorProvider[equipable.Model](err)
+		}
+		for _, e := range c.Inventory().Equipable().Items() {
+			if e.Slot() == slot {
+				return model2.FixedProvider(e)
+			}
+		}
+		return model2.ErrorProvider[equipable.Model](errors.New("equipable not found"))
+	}
+}
+
+func GetItemInSlot(l logrus.FieldLogger, span opentracing.Span, tenant tenant.Model) func(characterId uint32, inventoryType byte, slot int16) model2.Provider[item.Model] {
+	return func(characterId uint32, inventoryType byte, slot int16) model2.Provider[item.Model] {
+		// TODO this needs to be more performant
+		c, err := GetByIdWithInventory(l, span, tenant)(characterId)
+		if err != nil {
+			return model2.ErrorProvider[item.Model](err)
+		}
+
+		var inv func() inventory.ItemModel
+		switch inventoryType {
+		case 2:
+			inv = c.Inventory().Use
+		case 3:
+			inv = c.Inventory().Setup
+		case 4:
+			inv = c.Inventory().Etc
+		case 5:
+			inv = c.Inventory().Cash
+		}
+
+		for _, e := range inv().Items() {
+			if e.Slot() == slot {
+				return model2.FixedProvider(e)
+			}
+		}
+		return model2.ErrorProvider[item.Model](errors.New("item not found"))
 	}
 }
