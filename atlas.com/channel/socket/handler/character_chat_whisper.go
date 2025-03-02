c6d461b58d132cf0a9a757ccfe9641804037d2c2
Chronicle20 <a.chronicle.20@gmail.com>
2025-03-02 11:01:10 -0500

    Stub in find handling.

--- atlas.com/channel/socket/handler/character_chat_whisper.go
+++ atlas.com/channel/socket/handler/character_chat_whisper.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"atlas-channel/character"
 	"atlas-channel/message"
 	"atlas-channel/session"
 	"atlas-channel/socket/writer"
@@ -15,10 +16,12 @@ const CharacterChatWhisperHandle = "CharacterChatWhisperHandle"
 type WhisperMode byte
 
 const (
-	WhisperModeChat = WhisperMode(6)
+	WhisperModeFind       = WhisperMode(5)
+	WhisperModeChat       = WhisperMode(6)
+	WhisperModeFindFriend = WhisperMode(68)
 )
 
-func CharacterChatWhisperHandleFunc(l logrus.FieldLogger, ctx context.Context, _ writer.Producer) func(s session.Model, r *request.Reader, readerOptions map[string]interface{}) {
+func CharacterChatWhisperHandleFunc(l logrus.FieldLogger, ctx context.Context, wp writer.Producer) func(s session.Model, r *request.Reader, readerOptions map[string]interface{}) {
 	t := tenant.MustFromContext(ctx)
 	return func(s session.Model, r *request.Reader, readerOptions map[string]interface{}) {
 		mode := WhisperMode(r.ReadByte())
@@ -29,15 +32,37 @@ func CharacterChatWhisperHandleFunc(l logrus.FieldLogger, ctx context.Context, _
 		targetName := r.ReadAsciiString()
 		msg := ""
 
+		if mode == WhisperModeFind || mode == WhisperModeFindFriend {
+			tc, err := character.GetByName(l, ctx)(targetName)
+			if err != nil {
+				_ = session.Announce(l)(ctx)(wp)(writer.CharacterChatWhisper)(writer.CharacterChatWhisperFindResultErrorBody(targetName))(s)
+				return
+			}
+			// TODO query cash shop.
+			cs := false
+			if cs {
+				_ = session.Announce(l)(ctx)(wp)(writer.CharacterChatWhisper)(writer.CharacterChatWhisperFindResultInCashShopBody(targetName))(s)
+				return
+			}
+
+			_, err = session.GetByCharacterId(t)(tc.Id())
+			if err == nil {
+				_ = session.Announce(l)(ctx)(wp)(writer.CharacterChatWhisper)(writer.CharacterChatWhisperFindResultInMapBody(tc, tc.MapId()))(s)
+				return
+			}
+
+			// TODO find a way to look up remote channel.
+			_ = session.Announce(l)(ctx)(wp)(writer.CharacterChatWhisper)(writer.CharacterChatWhisperFindResultInOtherChannelBody(targetName, 0))(s)
+		}
 		if mode == WhisperModeChat {
 			msg = r.ReadAsciiString()
 			err := message.WhisperChat(l)(ctx)(s.Map(), s.CharacterId(), msg, targetName)
 			if err != nil {
-				// TODO whisper error response.
+				_ = session.Announce(l)(ctx)(wp)(writer.CharacterChatWhisper)(writer.CharacterChatWhisperSendFailureResultBody(targetName, false))(s)
 				return
 			}
 			return
 		}
-		l.Debugf("Character [%d] using whipser mode [%d]. Target [%s], Message [%s], UpdateTime [%d]", s.CharacterId(), mode, targetName, msg, updateTime)
+		l.Warnf("Character [%d] using unhandled whipser mode [%d]. Target [%s], Message [%s], UpdateTime [%d]", s.CharacterId(), mode, targetName, msg, updateTime)
 	}
 }
--- atlas.com/channel/socket/writer/character_chat_whisper.go
+++ atlas.com/channel/socket/writer/character_chat_whisper.go
@@ -9,15 +9,75 @@ const CharacterChatWhisper = "CharacterChatWhisper"
 
 type WhisperMode byte
 
+type WhisperFindResultMode byte
+
 const (
-	WhisperModeSend    = WhisperMode(0x0A)
-	WhisperModeReceive = WhisperMode(0x12)
+	WhisperModeSend       = WhisperMode(0x0A)
+	WhisperModeReceive    = WhisperMode(0x12)
+	WhisperModeFindResult = WhisperMode(0x09)
+
+	WhisperFindResultModeError            = WhisperFindResultMode(0)
+	WhisperFindResultModeMap              = WhisperFindResultMode(1)
+	WhisperFindResultModeCashShop         = WhisperFindResultMode(2)
+	WhisperFindResultModeDifferentChannel = WhisperFindResultMode(3)
+	WhisperFindResultModeUnable2          = WhisperFindResultMode(4)
 )
+
+func CharacterChatWhisperFindResultInCashShopBody(targetName string) BodyProducer {
+	return func(w *response.Writer, options map[string]interface{}) []byte {
+		w.WriteByte(byte(WhisperModeFindResult))
+		w.WriteAsciiString(targetName)
+		w.WriteByte(byte(WhisperFindResultModeCashShop))
+		w.WriteInt32(-1)
+		return w.Bytes()
+	}
+}
+
+func CharacterChatWhisperFindResultInMapBody(target character.Model, mapId uint32) BodyProducer {
+	return func(w *response.Writer, options map[string]interface{}) []byte {
+		w.WriteByte(byte(WhisperModeFindResult))
+		w.WriteAsciiString(target.Name())
+		w.WriteByte(byte(WhisperFindResultModeMap))
+		w.WriteInt(mapId)
+		w.WriteInt32(int32(target.X()))
+		w.WriteInt32(int32(target.Y()))
+		return w.Bytes()
+	}
+}
+
+func CharacterChatWhisperFindResultInOtherChannelBody(targetName string, channelId byte) BodyProducer {
+	return func(w *response.Writer, options map[string]interface{}) []byte {
+		w.WriteByte(byte(WhisperModeFindResult))
+		w.WriteAsciiString(targetName)
+		w.WriteByte(byte(WhisperFindResultModeDifferentChannel))
+		w.WriteInt(uint32(channelId))
+		return w.Bytes()
+	}
+}
+
+func CharacterChatWhisperFindResultErrorBody(targetName string) BodyProducer {
+	return func(w *response.Writer, options map[string]interface{}) []byte {
+		w.WriteByte(byte(WhisperModeFindResult))
+		w.WriteAsciiString(targetName)
+		w.WriteByte(byte(WhisperFindResultModeError))
+		w.WriteInt(0)
+		return w.Bytes()
+	}
+}
 
 func CharacterChatWhisperSendResultBody(target character.Model, success bool) BodyProducer {
 	return func(w *response.Writer, options map[string]interface{}) []byte {
 		w.WriteByte(byte(WhisperModeSend))
 		w.WriteAsciiString(target.Name())
+		w.WriteBool(success)
+		return w.Bytes()
+	}
+}
+
+func CharacterChatWhisperSendFailureResultBody(targetName string, success bool) BodyProducer {
+	return func(w *response.Writer, options map[string]interface{}) []byte {
+		w.WriteByte(byte(WhisperModeSend))
+		w.WriteAsciiString(targetName)
 		w.WriteBool(success)
 		return w.Bytes()
 	}
